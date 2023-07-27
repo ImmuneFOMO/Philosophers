db0a29e00f81bb98338945e55fe8471df790fe3d
ImmuneFOMO <rikiton.shift@gmail.com>
2023-07-27 02:41:30 +0100

    init: start project and create all files that i need

---
+++ philo/h_files/philosopher.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azhadan <azhadan@student.42lisboa.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 01:52:18 by azhadan           #+#    #+#             */
+/*   Updated: 2023/07/27 02:38:39 by azhadan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_H
+# define PHILOSOPHER_H
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+
+#endif
