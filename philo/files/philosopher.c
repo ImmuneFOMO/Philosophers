16d5976031245ce52c83df689ad58857e11bcd84
ImmuneFOMO <rikiton.shift@gmail.com>
2023-07-29 20:49:36 +0100

    feat: add check for args if it num

--- philo/files/philosopher.c
+++ philo/files/philosopher.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosopher.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azhadan <azhadan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: azhadan <azhadan@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 01:51:17 by azhadan           #+#    #+#             */
-/*   Updated: 2023/07/29 18:15:36 by azhadan          ###   ########.fr       */
+/*   Updated: 2023/07/29 20:48:45 by azhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,16 +36,45 @@ void	start_hands(hands_t **hands)
 	pthread_mutex_init(&(*hands)->right_h, NULL);
 }
 
+int	ft_isnums(char **str)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (str[i])
+	{
+		j = 0;
+		while (str[i][j])
+		{
+			if (str[i][j] > '9' || str[i][j] < '0')
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+void ft_check_args(char **argv)
+{
+	if (ft_isnums(argv))
+	{
+		printf("is not num\n");
+		exit(1);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	pthread_t	t_one;
 	pthread_t	t_two;
 	hands_t		*hands;
 
-	(void)argc;
 	(void)argv;
-	if (argc == 3)
+	if (argc >= 5 && argc <= 6)
 	{
+		ft_check_args(argv);
 		hands = (hands_t *)malloc(sizeof(hands_t));
 		if (!hands)
 			exit(1);
@@ -59,5 +88,6 @@ int	main(int argc, char **argv)
 		pthread_mutex_destroy(&hands->right_h);
 		free(hands);
 	}
+	printf("return 0\n");
 	return (0);
 }
