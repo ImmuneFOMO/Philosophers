d70ef047b14615f1da412b0486ae360ed602dd32
trelixon <rikiton.shift@gmail.com>
2023-07-31 16:00:23 +0100

    feat: add time checker

--- philo/files/philosopher.c
+++ philo/files/philosopher.c
@@ -6,7 +6,7 @@
 /*   By: azhadan <azhadan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 01:51:17 by azhadan           #+#    #+#             */
-/*   Updated: 2023/07/30 23:17:58 by azhadan          ###   ########.fr       */
+/*   Updated: 2023/07/31 15:59:17 by azhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ int	ft_isnums(char **str)
 	return (0);
 }
 
-void	ft_check_args(char **argv)
+void	ft_check_args(char **argv, hands_t **philos)
 {
 	int	i;
 
@@ -68,22 +68,28 @@ void	ft_check_args(char **argv)
 	}
 	while (argv[++i])
 	{
-		if (ft_atoi(argv[i]) <= 0)
+		if ((ft_atoi(argv[i]) <= 0) || (ft_atoi(argv[i]) > 2147483647
+				|| ft_atoi(argv[i]) < -2147483648))
 			exit(1);
 	}
+	*philos = (hands_t *)malloc(sizeof(hands_t));
+	(*philos)->num_philo = ft_atoi(argv[1]);
 }
 
 int	main(int argc, char **argv)
 {
 	pthread_t	t_one;
 	pthread_t	t_two;
+	uint64_t	time;
+	uint64_t	time_2;
 	hands_t		*hands;
 
 	(void)argv;
 	if (argc >= 5 && argc <= 6)
 	{
-		ft_check_args(argv);
-		hands = (hands_t *)malloc(sizeof(hands_t));
+		current_time(&time);
+		printf("%llu\n", time);
+		ft_check_args(argv, &hands);
 		if (!hands)
 			exit(1);
 		start_hands(&hands);
@@ -92,8 +98,13 @@ int	main(int argc, char **argv)
 		pthread_join(t_one, NULL);
 		pthread_join(t_two, NULL);
 		printf("result:%d\n", hands->test);
+		printf("test %d\n", hands->num_philo);
 		pthread_mutex_destroy(&hands->left_h);
 		pthread_mutex_destroy(&hands->right_h);
+		usleep(100000);
+		current_time(&time_2);
+		printf("%llu\n", time_2);
+		printf("past:%llu\n", time_2 - time);
 		free(hands);
 	}
 	printf("return (0)\n");
--- philo/h_files/philosopher.h
+++ philo/h_files/philosopher.h
@@ -6,13 +6,14 @@
 /*   By: azhadan <azhadan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 01:52:18 by azhadan           #+#    #+#             */
-/*   Updated: 2023/07/30 09:31:32 by azhadan          ###   ########.fr       */
+/*   Updated: 2023/07/31 15:53:05 by azhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHER_H
 # define PHILOSOPHER_H
 # include <pthread.h>
+# include <stdint.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
@@ -21,15 +22,17 @@
 
 typedef struct hands
 {
+	int				num_philo;
 	pthread_mutex_t	left_h;
 	pthread_mutex_t	right_h;
 	int				test;
 }					hands_t;
 
 //philosopher.c
-void				ft_check_args(char **argv);
+void				ft_check_args(char **argv, hands_t **philos);
 int					ft_isnums(char **str);
 //helpers.c
 long long			ft_atoi(const char *str);
+void				current_time(uint64_t *fill);
 
 #endif
