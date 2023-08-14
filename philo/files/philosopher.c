efe093f6b726cb45cb4a11b4dc2a8360158e9c91
ImmuneFOMO <rikiton.shift@gmail.com>
2023-08-14 20:08:13 +0100

    feat: last stable version, there work with limits, 1 philo, inifinity run

--- philo/files/philosopher.c
+++ philo/files/philosopher.c
@@ -6,7 +6,7 @@
 /*   By: azhadan <azhadan@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 01:51:17 by azhadan           #+#    #+#             */
-/*   Updated: 2023/08/14 18:39:47 by azhadan          ###   ########.fr       */
+/*   Updated: 2023/08/14 20:02:23 by azhadan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,14 +69,21 @@ void	*start_life(void *arg)
 		philo_print(philo, "has taken a fork", 1);
 		pthread_mutex_lock(&philo->global->forks[philo->right_hand]);
 		philo_print(philo, "has taken a fork", 1);
-		if (philo->counter_fed == philo->global->num_times_feed)
+		if (philo->global->num_times_feed > 0 && \
+		philo->counter_fed == philo->global->num_times_feed)
+		{
+			pthread_mutex_unlock(&philo->global->forks[philo->left_hand]);
+			pthread_mutex_unlock(&philo->global->forks[philo->right_hand]);
 			break ;
+		}
 		pthread_mutex_lock(&philo->global->checker);
 		philo_print(philo, "is eating", 1);
 		philo->time_last_food = current_time();
 		pthread_mutex_unlock(&philo->global->checker);
 		ft_custom_sleep(philo->global->time_to_eat, philo->global);
+		pthread_mutex_lock(&philo->global->checker);
 		philo->counter_fed++;
+		pthread_mutex_unlock(&philo->global->checker);
 		pthread_mutex_unlock(&philo->global->forks[philo->left_hand]);
 		pthread_mutex_unlock(&philo->global->forks[philo->right_hand]);
 		philo_print(philo, "is sleeping", 1);
@@ -101,8 +108,6 @@ int	ft_start_philo(t_global *global)
 		return (1);
 	}
 	i = 0;
-	pthread_mutex_init(&global->printf, NULL);
-	pthread_mutex_init(&global->checker, NULL);
 	while (i < global->num_philo)
 	{
 		if (pthread_mutex_init(&global->forks[i], NULL))
@@ -111,13 +116,19 @@ int	ft_start_philo(t_global *global)
 			free(global->forks);
 			return (1);
 		}
+		i++;
+	}
+	pthread_mutex_init(&global->printf, NULL);
+	pthread_mutex_init(&global->checker, NULL);
+	global->start_time = current_time();
+	i = 0;
+	while (i < global->num_philo)
+	{
 		global->person[i].left_hand = i;
 		global->person[i].right_hand = (i + 1) % global->num_philo;
 		global->person[i].id = i + 1;
 		global->person[i].global = global;
 		global->person[i].counter_fed = 0;
-		if (global->num_philo > 1)
-			global->start_time = current_time();
 		global->person[i].time_last_food = current_time();
 		if (pthread_create(&global->person[i].th, NULL, &start_life,
 				&global->person[i]))
